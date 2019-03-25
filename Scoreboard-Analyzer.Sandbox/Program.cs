206c33dc63984bc5631e657996bee606736f48d2
Christian Campana-Emard <christian.campana.emard@outlook.com>
2019-03-25 11:14:38 -0400

    made sandbox

---
+++ Scoreboard-Analyzer.Sandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scoreboard_Analyzer.Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
